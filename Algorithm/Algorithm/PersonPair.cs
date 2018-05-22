2c5bc588a17f302b0502a6a1e0fc504afd2a7776
Justin Morgan <justin.morgan.123@gmail.com>
2018-05-22 00:54:24 -0400

    Comments

--- Algorithm/Algorithm/PersonPair.cs
+++ Algorithm/Algorithm/PersonPair.cs
@@ -7,8 +7,8 @@ namespace Algorithm
     //   added another constructor, and provided the Empty method for the trivial case of a pair with no people in it. Strictly speaking,
     //   this makes it more than just refactoring since I'm changing the interface of a class used outside this assembly, but the rules 
     //   seem to make it fair game. 
-    // - The idea here is that since externally changing any of its properties could put a PersonPair into an invalid state, the class 
-    //   should encapsulate their logic and enforce its own validity. 
+    // - The idea here, of course, is that since externally changing any of its properties could put a PersonPair into an invalid state, 
+    //   the class should encapsulate their logic and enforce its own validity. 
     // - In addition to the Empty method, I've made AgeDifference nullable so it defaults to null for an empty pair. This is to 
     //   emphasize that an empty pair is a special case and can't be trusted to hold meaningful information. If the unit tests didn't
     //   expect it, I would prefer to return a null PersonPair instead of the deceptive ghost value.
